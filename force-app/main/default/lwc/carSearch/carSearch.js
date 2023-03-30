49ca145a3641536f0d0ee810bd703c55997ae24a
nsuram931 <suram.narendar@gmail.com>
2023-03-30 16:28:28 -0400

    JIRA-123-Added New Field

--- force-app/main/default/lwc/carSearch/carSearch.js
+++ force-app/main/default/lwc/carSearch/carSearch.js
@@ -4,6 +4,7 @@ export default class CarSearch extends LightningElement {
 @track carTypeId
     carTypeSelectHandler(event){
         this.carTypeId=event.detail;
+        console.log('Test123');
 
         
        
